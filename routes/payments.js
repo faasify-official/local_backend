860a1ce9c3a279a6cf94aa7fb55ea24d3647fd34
Darrel Tapilaha <tapsdarrel@gmail.com>
2025-11-27 22:14:54 -0800

    Modified Stripe checkout to use credentials stored in AWS Secrets Manager

--- routes/payments.js
+++ routes/payments.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const Stripe = require('stripe')
 const { verifyToken } = require('../utils/jwt')
+const { getSecret } = require('../utils/secrets')
 const router = express.Router()
 
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-06-20',
-})
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+//   apiVersion: '2024-06-20',
+// })
+
+let stripeClient = null
+async function getStripeClient() {
+  if (!stripeClient) {
+    const key = await getSecret('STRIPE_SECRET_KEY')
+    stripeClient = new Stripe(key, {
+      apiVersion: '2024-06-20',
+    })
+  }
+  return stripeClient
+}
 
 router.post('/create-payment-intent', async (req, res) => {
   const user = await verifyToken(req)
   if (!user) {
     return res.status(401).json({ error: 'Unauthorized' })
   }
+
+  const stripe = await getStripeClient();
 
   const { amount, currency, orderId } = req.body
 
